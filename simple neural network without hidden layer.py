7ab8394376831b1e3014292c0a9a5d1b9f0b6905
Chansung Park <deep.diver.csp@gmail.com>
2018-04-02 16:17:26 +0900

    Update simple neural network without hidden layer.py

--- simple neural network without hidden layer.py
+++ simple neural network without hidden layer.py
@@ -200,6 +200,8 @@ def main():
     d = model(train_set_x, train_set_y,
                 test_set_x, test_set_y,
                 num_iterations = 2000, learning_rate = 0.005, print_cost = True)
+    
+    print(d)
 
 if __name__ == "__main__":
     main()
